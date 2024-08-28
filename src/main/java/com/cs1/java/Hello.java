54826dcfe5ac7d3503585083dcc6905efefc42d6
Acarrillo-7 <acarrillo7@unl.edu>
2024-08-28 14:21:00 -0500

    added andrew

--- src/main/java/com/cs1/java/Hello.java
+++ src/main/java/com/cs1/java/Hello.java
@@ -4,5 +4,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Max Schessler");
         System.out.println("I am a computer engineering major");
+        
+        System.out.println("My name is Andrew Carrillo");
+        System.out.println("I am a computer engineerinng major");
+
     }
 }
